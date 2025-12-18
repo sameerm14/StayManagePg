65b8fc6816374413cfa8324b3e41c40b57da3557
Sameer Nadaf <130203947+sameerm14@users.noreply.github.com>
2025-12-18 15:36:54 +0530

    Add IOException import to TenantsFeatureService

--- StayManage/src/main/java/com/pg/StayManage/Service/TenantsFeatureService.java
+++ StayManage/src/main/java/com/pg/StayManage/Service/TenantsFeatureService.java
@@ -14,6 +14,7 @@ import com.pg.StayManage.Repository.TenantRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
+import java.io.IOException;
 
 import java.time.LocalDate;
 import java.util.List;
