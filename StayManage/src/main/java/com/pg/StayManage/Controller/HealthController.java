214d524a403201545a493da57cc50e3e217c0d71
Sameer Nadaf <130203947+sameerm14@users.noreply.github.com>
2025-12-19 10:56:11 +0530

    Update HealthController.java

--- StayManage/src/main/java/com/pg/StayManage/Controller/HealthController.java
+++ StayManage/src/main/java/com/pg/StayManage/Controller/HealthController.java
@@ -1,1 +1,13 @@
 
+package com.pg.StayManage.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+
+    @GetMapping("/api/health")
+    public String health() {
+        return "OK";
+    }
+}
