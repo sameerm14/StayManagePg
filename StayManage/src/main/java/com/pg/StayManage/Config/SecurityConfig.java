7acbb68a40594218c579c69220ae730502babc57
Sameer Nadaf <130203947+sameerm14@users.noreply.github.com>
2025-12-24 19:03:53 +0530

    Update security roles for tenant features and meals

--- StayManage/src/main/java/com/pg/StayManage/Config/SecurityConfig.java
+++ StayManage/src/main/java/com/pg/StayManage/Config/SecurityConfig.java
@@ -39,7 +39,7 @@ public class SecurityConfig {
                                                 "/payment/**", "/api/notification/**")
 
                                 .hasRole("ADMIN")
-                                .requestMatchers("/tenant/features/**")
+                                .requestMatchers("/tenant/features/**","/api/food/allmeals")
                                 .hasRole("TENANT")
                                 .anyRequest()
                                 .authenticated());
