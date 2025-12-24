6ac36e7f422c6149d14334cd32d6991b22e81a03
Sameer Nadaf <130203947+sameerm14@users.noreply.github.com>
2025-12-24 18:15:33 +0530

    Update tenant features request matcher for security

--- StayManage/src/main/java/com/pg/StayManage/Config/SecurityConfig.java
+++ StayManage/src/main/java/com/pg/StayManage/Config/SecurityConfig.java
@@ -39,7 +39,7 @@ public class SecurityConfig {
                                                 "/payment/**", "/api/notification/**")
 
                                 .hasRole("ADMIN")
-                                .requestMatchers("/tenant/features")
+                                .requestMatchers("/tenant/features/**")
                                 .hasRole("TENANT")
                                 .anyRequest()
                                 .authenticated());
