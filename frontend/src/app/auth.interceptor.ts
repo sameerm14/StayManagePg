661f37badfc0dbe8be424f79baba62ab9a376535
Sameer Nadaf <130203947+sameerm14@users.noreply.github.com>
2025-12-24 13:04:10 +0530

    Change localStorage key from 'authToken' to 'token'

--- frontend/src/app/auth.interceptor.ts
+++ frontend/src/app/auth.interceptor.ts
@@ -13,7 +13,7 @@ export class AuthInterceptor implements HttpInterceptor {
     req: HttpRequest<any>,
     next: HttpHandler
   ): Observable<HttpEvent<any>> {
-    const token = localStorage.getItem('authToken');
+    const token = localStorage.getItem('token');
 
     if (token) {
       const clonedRequest = req.clone({
