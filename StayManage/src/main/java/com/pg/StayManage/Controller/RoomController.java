5b579c5aadcf1ac7913c6e394e8af66c9552b1b9
Sameer Nadaf <130203947+sameerm14@users.noreply.github.com>
2026-01-05 23:15:42 +0530

    Update RoomController.java

--- StayManage/src/main/java/com/pg/StayManage/Controller/RoomController.java
+++ StayManage/src/main/java/com/pg/StayManage/Controller/RoomController.java
@@ -6,6 +6,7 @@ import com.pg.StayManage.Service.RoomService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import com.pg.StayManage.Model.Tenant;
 import org.springframework.web.bind.annotation.*;
 import com.pg.StayManage.Service.TenantService;
 import org.springframework.web.multipart.MultipartFile;
