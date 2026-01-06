78ef27a07dfcf33b93623b5c49cbbc3f16d87973
Sameer Nadaf <130203947+sameerm14@users.noreply.github.com>
2026-01-06 18:12:41 +0530

    Update tenantlogin.component.ts

--- frontend/src/app/tenantlogin/tenantlogin.component.ts
+++ frontend/src/app/tenantlogin/tenantlogin.component.ts
@@ -43,7 +43,7 @@ export class TenantloginComponent {
         }, 3000);
       },
       error: (error) => {
-        this.successMessage = 'Login failed. Please try again.';
+        this.successMessage = 'Login failed or Your account is not activated by admin.';
         this.messageColor = 'red';
       },
     });
